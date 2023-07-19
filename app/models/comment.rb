912bc853e49ed6f01422721e9c322ef350f68dfc
jlvFlores <jluisvazquez.jv@gmail.com>
2023-07-19 11:55:20 -0600

    Add model methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
-  belongs_to :post
+  belongs_to :post, counter_cache: true
   belongs_to :author, class_name: "User"
+  
+  after_create :update_post_comments_count
+
+  private
+
+  def update_post_comments_count
+    post.update(comments_count: post.comments.count)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
-  belongs_to :post
+  belongs_to :post, counter_cache: true
   belongs_to :author, class_name: "User"
+
+  after_create :update_post_likes_count
+
+  private
+
+  def update_post_likes_count
+    post.update(likes_count: post.likes.count)
+  end
 end
