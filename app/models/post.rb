3969b662f0a4c58151936c08c6a0c6d809871463
jlvFlores <jluisvazquez.jv@gmail.com>
2023-07-19 13:12:15 -0600

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ApplicationRecord
 
   after_save :update_user_posts_count
 
+  validates :title, presence: true, length: { maximum: 250}
+  validates :comments_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def recent_comments
     Comment.where(post_id: id).order(created_at: :desc).limit(5)
   end
