affe117b656ef0d30251c6320d7ddad6d8058e97
jlvFlores <jluisvazquez.jv@gmail.com>
2023-07-26 10:05:43 -0600

    Add new comment form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :posts, only: [:new, :create]
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:new, :create]
+    end
   end
 end
