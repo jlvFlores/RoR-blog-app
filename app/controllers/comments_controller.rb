6fb0c609a8536cc5d3168ec3e190666b5735aa95
jlvFlores <jluisvazquez.jv@gmail.com>
2023-07-26 13:07:35 -0600

    Add new comment view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,18 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
     @comment.author = current_user
 
-    return unless @comment.save
-
-    redirect_to user_post_path(@post.author, @post)
+    if @comment.save
+      redirect_to user_post_path(@post.author, @post)
+    else
+      render :_new
+    end
   end
 
   private
