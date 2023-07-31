9cd16e75bdc81abf8c0c435149ee9c94d50aeb41
oovillagran <oscar.villagran@hotmail.com>
2023-07-31 11:06:25 -0500

    Add link references change in _post_box.html.erb to author_id instead of author.id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = Post.where(author_id: params[:user_id]).order(created_at: :desc)
+    @posts = Post.includes(:comments, :author).where(author_id: params[:user_id]).order(created_at: :desc)
   end
 
   def show
