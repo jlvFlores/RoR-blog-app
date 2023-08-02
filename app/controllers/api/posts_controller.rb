b324424c8257e71fb831fffcd55606122e767990
oovillagran <oscar.villagran@hotmail.com>
2023-08-02 17:30:58 -0500

    Add comments method in app/controllers/api/posts_controller.rb

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,13 +1,18 @@
 class Api::PostsController < ApplicationController
+  def comments
+    @post = Post.find(params[:id])
+    render json: { comments: @post.comments }
+  end
+
   def add_comment
     @post = Post.find(params[:id])
     @comment = @post.comments.build(text: params[:text])
     @comment.author = current_user
 
     if @comment.save
-      render json: { message: "Comment added successfully" }, status: :created
+      render json: { message: 'Comment added successfully' }, status: :created
     else
-      render json: { error: "Failed to add comment" }, status: :unprocessable_entity
+      render json: { error: 'Failed to add comment' }, status: :unprocessable_entity
     end
   end
 end
